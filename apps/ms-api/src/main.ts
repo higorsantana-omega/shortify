293472672b25eeceaf5801ba691357c68d7333b3
higorsantana-omega <higor.santanahs17@gmail.com>
2025-05-13 19:23:13 -0300

    refactor(ms-api/main): replace hardcoded PORT

--- apps/ms-api/src/main.ts
+++ apps/ms-api/src/main.ts
@@ -1,8 +1,15 @@
+/* eslint-disable perfectionist/sort-imports */
+import 'dotenv/config'
+
 import { NestFactory } from '@nestjs/core'
+
 import { AppModule } from './app.module'
+
+import { env } from './shared/config/env'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+  await app.listen(env.APP_PORT)
 }
-bootstrap()
+
+void bootstrap()
