3a989fb50536d900f567b05e31d41e689060080a
higorsantana-omega <higor.santanahs17@gmail.com>
2025-05-14 14:59:15 -0300

    feat(web): add wrap layout with DashboardProviders

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+import { DashboardProviders } from './providers'
+
 import { Sidebar } from './_components/sidebar'
 
 export default function DashboardLayout({
@@ -6,15 +8,17 @@ export default function DashboardLayout({
   children: React.ReactNode
 }>) {
   return (
-    <div className='flex'>
-      <Sidebar />
-      <main className='flex-1'>
-        <div className="flex-1 bg-background min-h-screen">
-          <div className="max-w-screen-xl mx-auto px-4 py-6">
-            {children}
+    <DashboardProviders>
+      <div className='flex'>
+        <Sidebar />
+        <main className='flex-1'>
+          <div className="flex-1 bg-background min-h-screen">
+            <div className="max-w-screen-xl mx-auto px-4 py-6">
+              {children}
+            </div>
           </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+    </DashboardProviders>
   )
 }
