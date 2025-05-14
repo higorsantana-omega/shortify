2984cc48e1d3e9f2c332e4358fa6d6185f173a36
higorsantana-omega <higor.santanahs17@gmail.com>
2025-05-14 19:20:11 -0300

    feat(web): introduce LinksList component and fetch links data using getLinks action

--- apps/web/app/(dashboard)/links/page.tsx
+++ apps/web/app/(dashboard)/links/page.tsx
@@ -1,12 +1,19 @@
-import { LinkHeader } from "./_components/link-header"
-import { NewLinkModal } from "./_components/modals/new-link-modal"
+import { getLinks } from '../../../lib/actions/data/links'
+import { LinkHeader } from './_components/link-header'
+import { LinksList } from './_components/links-list'
 
-export default function LinksPage() {
+import { NewLinkModal } from './_components/modals/new-link-modal'
+
+export default async function LinksPage() {
+  const result = await getLinks()
+
   return (
     <>
       <LinkHeader />
 
       <NewLinkModal />
+
+      <LinksList response={result} />
     </>
   )
 }
