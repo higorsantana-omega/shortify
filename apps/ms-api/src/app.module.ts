9265308075dc36eb2e11db46f3124194ff2e402d
higorsantana-omega <higor.santanahs17@gmail.com>
2025-05-14 10:55:41 -0300

    feat(ms-api): add LinksModule to the imports and register ZodValidationPipe as a provider

--- apps/ms-api/src/app.module.ts
+++ apps/ms-api/src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common'
 
+import { APP_PIPE } from '@nestjs/core'
+import { ZodValidationPipe } from 'nestjs-zod'
+
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-
+import { LinksModule } from './modules/links/links.module'
 import { DatabaseModule } from './shared/module/database/database.module'
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, LinksModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidationPipe,
+    },
+    AppService,
+  ],
 })
 export class AppModule {}
