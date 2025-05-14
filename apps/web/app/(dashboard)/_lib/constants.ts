06ce25001ef1a96ef73b986c5b097da5ab5c7cea
higorsantana-omega <higor.santanahs17@gmail.com>
2025-05-14 14:08:40 -0300

    feat(web): add sidebar items and getIcon function

---
+++ apps/web/app/(dashboard)/_lib/constants.ts
@@ -0,0 +1,10 @@
+export interface SidebarItem {
+  name: string
+  icon: string
+  path: string
+}
+
+export const sidebarItems: SidebarItem[] = [
+  { name: 'Links', icon: 'link', path: '/links' },
+  { name: 'Analytics', icon: 'chart', path: '/analytics' }
+]
---
+++ apps/web/app/(dashboard)/_lib/get-icon.tsx
@@ -0,0 +1,12 @@
+import { BarChart3, Link2 } from 'lucide-react'
+
+export const getIcon = (icon: string) => {
+  switch (icon) {
+    case 'link':
+      return <Link2 className='h-4 w-4' />
+    case 'chart':
+      return <BarChart3 className='h-4 w-4' />
+    default:
+      return <Link2 className='h-4 w-4' />
+  }
+}
