33e41a5d5dd382a584f481306002c2d42660dfed
higorsantana-omega <higor.santanahs17@gmail.com>
2025-05-14 19:22:13 -0300

    feat(ms-api): update default domain for newLinkDto to 'localhost:3006'

--- apps/ms-api/src/modules/links/dtos/new-link.dto.ts
+++ apps/ms-api/src/modules/links/dtos/new-link.dto.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 
 const newLinkDto = z.object({
   key: z.string().optional(),
-  domain: z.string().optional().default('localhost:3001'),
+  domain: z.string().optional().default('localhost:3006'),
   url: z.string(),
 })
 
