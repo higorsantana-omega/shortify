835992c83ecefb0514027e18a9fc12896c2f090c
higorsantana-omega <higor.santanahs17@gmail.com>
2025-05-14 12:11:24 -0300

    feat(ms-redirect): add DatabaseModule and RedirectModule imports

--- apps/ms-redirect/src/app.module.ts
+++ apps/ms-redirect/src/app.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+
+import { APP_PIPE } from '@nestjs/core'
+import { ZodValidationPipe } from 'nestjs-zod'
+
+import { RedirectModule } from './modules/redirect/redirect.module'
+import { DatabaseModule } from './shared/module/database/database.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DatabaseModule, RedirectModule],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidationPipe,
+    },
+  ],
 })
 export class AppModule {}
