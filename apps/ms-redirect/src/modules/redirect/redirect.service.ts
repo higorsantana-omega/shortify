67ab701100282e991de63bb75c2c227d30875f40
higorsantana-omega <higor.santanahs17@gmail.com>
2025-05-14 12:09:56 -0300

    feat(ms-redirect): create redirect service to handle URL redirection by key and check for expiration

---
+++ apps/ms-redirect/src/modules/redirect/redirect.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { LinksRepository } from 'src/shared/module/database/repositories/links.repository'
+
+@Injectable()
+export class RedirectService {
+  constructor(private readonly linksRepository: LinksRepository) {}
+
+  async getUrlByKey(key: string): Promise<string> {
+    const link = await this.linksRepository.getByKey(key)
+
+    if (!link)
+      throw new NotFoundException('Link not found')
+
+    const isExpired = link.checkIfExpired()
+    if (isExpired)
+      throw new NotFoundException('Link not found')
+
+    return link.getUrl()
+  }
+}
