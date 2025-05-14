ed3b955b6ec6b9244c0f7b308eeb89391018628b
higorsantana-omega <higor.santanahs17@gmail.com>
2025-05-14 12:11:38 -0300

    chore: setup app port

--- apps/ms-redirect/src/main.ts
+++ apps/ms-redirect/src/main.ts
@@ -1,8 +1,16 @@
+/* eslint-disable perfectionist/sort-imports */
+import 'dotenv/config'
+
 import { NestFactory } from '@nestjs/core'
+
 import { AppModule } from './app.module'
+
+import { env } from './shared/config/env'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+
+  await app.listen(env.APP_PORT)
 }
-bootstrap()
+
+void bootstrap()
