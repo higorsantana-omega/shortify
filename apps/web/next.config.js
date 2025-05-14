a71639a5686216d8637c387f45837c5abbc9082f
higorsantana-omega <higor.santanahs17@gmail.com>
2025-05-14 19:20:49 -0300

    chore(web): add @shortify/utils to transpilePackages

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@shortify/ui'],
+  transpilePackages: ['@shortify/ui', '@shortify/utils'],
 }
 
 export default nextConfig
