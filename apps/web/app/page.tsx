4820adb965123e8f8d025a38d2cf8d38cad96739
higorsantana-omega <higor.santanahs17@gmail.com>
2025-05-14 14:07:25 -0300

    feat(web): configure root layout

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,7 @@
-import styles from './page.module.css'
-
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        Main
-      </main>
-      <footer className={styles.footer}>
-        Footer
-      </footer>
+    <div>
+      not found
     </div>
   )
 }
