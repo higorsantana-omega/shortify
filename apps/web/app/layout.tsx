5f0c8aa408f7b9146ca9a3d4b7219fb58952485a
higorsantana-omega <higor.santanahs17@gmail.com>
2025-05-15 13:28:27 -0300

    feat(web): remove font

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from 'next'
-import localFont from 'next/font/local'
+// import localFont from 'next/font/local'
 import { Toaster } from 'react-hot-toast'
 import '@shortify/ui/globals.css'
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-})
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-})
 
 export const metadata: Metadata = {
   title: 'Shortify',
@@ -24,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         {children}
         <Toaster
           position='bottom-right'
